e38288ebe565053763e59477853e898a64e14dc4
이석호 <mercen@mercen.net>
2022-07-11 11:48:51 +0900

    Completed Search UI

--- Allermi/ContentView.swift
+++ Allermi/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 public let scs = UIScreen.main.bounds.size.height
 public let scw = UIScreen.main.bounds.size.width
 
+struct ColorManager {
+    static let LightColor = Color("LightColor")
+}
+
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
     @State var selectedIndex = 2
     let icons = ["house.fill", "bubble.left.and.bubble.right.fill", "magnifyingglass", "heart.text.square.fill", "person.fill"]
     let names = ["홈", "커뮤니티", "", "상태", "프로필"]
@@ -28,28 +33,37 @@ struct ContentView: View {
             }
             .padding(EdgeInsets(top: 0, leading: 0, bottom: scs/15, trailing: 0))
             
-            HStack {
-                ForEach(0..<5, id: \.self) { number in
-                    Spacer()
-                    Button(action: { self.selectedIndex = number }, label: {
-                        if number == 2 { Image(systemName: icons[number])
-                                .font(.system(size: scs/25, weight: .regular, design: .default))
-                                .foregroundColor(Color(.systemBackground)).frame(width: scs/12, height: scs/12)
-                                .background(selectedIndex == number ? Color.accentColor : .gray).cornerRadius(scs/10)
-                        }
-                        else {
-                            VStack {
-                                Image(systemName: icons[number])
-                                    .font(.system(size: number == 0 ? scs/25 : number == 1 ? scs/30 : scs/23, weight: .regular, design: .default))
-                                .frame(width: scs/25, height: scs/25)
-                                .foregroundColor(selectedIndex == number ? .accentColor : .gray)
-                                Text(names[number]).foregroundColor(selectedIndex == number ? .accentColor : .gray).font(.system(size: 10, weight: .bold, design: .default))
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(height: 1)
+                HStack {
+                    ForEach(0..<5, id: \.self) { number in
+                        Spacer()
+                        Button(action: { self.selectedIndex = number }, label: {
+                            if number == 2 { Image(systemName: icons[number])
+                                    .font(.system(size: scs/25, weight: .regular, design: .default))
+                                    .foregroundColor(Color(.systemBackground)).frame(width: scs/12, height: scs/12)
+                                    .background(selectedIndex == number ? Color.accentColor : .gray).cornerRadius(scs/10)
                             }
-                        }
-                    })
-                    Spacer()
+                            else {
+                                VStack {
+                                    Image(systemName: icons[number])
+                                        .font(.system(size: number == 0 ? scs/25 : number == 1 ? scs/30 : scs/23, weight: .regular, design: .default))
+                                    .frame(width: scs/25, height: scs/25)
+                                    .foregroundColor(selectedIndex == number ? .accentColor : .gray)
+                                    Text(names[number])
+                                        .foregroundColor(selectedIndex == number ? .accentColor : .gray).font(.system(size: 10, weight: .bold, design: .default))
+                                }
+                            }
+                        })
+                        Spacer()
+                    }
                 }
-            }.offset(x: 0, y: scs < 800 ? scs/2.2 : scs/2.33).padding(EdgeInsets(top: 0, leading: scs/100, bottom: scs < 800 ? 30 : 20, trailing: scs/100))
+                .padding(EdgeInsets(top: scs < 800 ? 5 : 10, leading: scs/100, bottom: scs < 800 ? 30 : 30, trailing: scs/100))
+                .background(colorScheme == .dark ? Color.gray.opacity(0.3) : Color.gray.opacity(0.1))
+            }
+            .offset(x: 0, y: scs < 800 ? scs/2.2 : scs/2.33)
         }
     }
 }
--- Allermi/SearchView.swift
+++ Allermi/SearchView.swift
@@ -26,22 +26,51 @@ struct SuperTextField: View {
     }
 }
 
+struct Tools: View {
+    var image: String
+    var title: String
+    var destinations: String
+    var body: some View {
+        VStack {
+            Button(action: {print("안뇽")}) {
+                Image(systemName: image)
+                    .font(.system(size: 27))
+                    .foregroundColor(Color(UIColor.systemBackground))
+            }
+                .frame(width: 55, height: 55)
+                .background(ColorManager.LightColor)
+                .clipShape(Circle())
+            Text(title)
+                .font(.system(size: 14, weight: .bold, design: .default))
+                .foregroundColor(.gray)
+        }.frame(width: 80)
+    }
+}
+
 struct SearchView: View {
+    @Environment(\.colorScheme) var colorScheme
     @State var text: String = ""
+    let icons = ["link", "location.fill", "barcode", "mic.fill"]
+    let titles = ["링크로 검색", "위치로 검색", "바코드 검색", "음성 검색"]
     var body: some View {
         VStack {
             Image("Logo")
                 .resizable()
                 .scaledToFit()
-                .padding(EdgeInsets(top: 0, leading: 80, bottom: 25, trailing: 80))
-                SuperTextField(placeholder: Text("식품명 또는 식당 상호명을 입력하세요"), text: $text)
+                .padding(EdgeInsets(top: 0, leading: 70, bottom: 45, trailing: 70))
+            SuperTextField(placeholder: Text("식품명 또는 식당 상호명을 입력하세요"), text: $text)
                 .frame(height: 60)
                 .multilineTextAlignment(.center)
                 .background(RoundedRectangle(cornerRadius: 24)
                     .strokeBorder(Color.gray, lineWidth: 1)
-                    .background(Color(.systemGray6)))
+                    .background(colorScheme == .dark ? Color.gray.opacity(0.3) : Color.gray.opacity(0.1)))
                 .clipShape(RoundedRectangle(cornerRadius: 24))
-                .padding(20)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 15, trailing: 20))
+            HStack {
+                ForEach(1..<5, id: \.self) { number in
+                    if number != 1 { Spacer() }
+                    Tools(image: icons[number-1], title: titles[number-1], destinations: "\(number)") }
+            }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
         }
     }
 }
